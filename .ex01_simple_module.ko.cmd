1ff69210c49c7521d6004f1f02b8a9bcedac4809
John Z <zhjohn925@gmail.com>
2016-06-29 22:10:55 -0700

    test

---
+++ .ex01_simple_module.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vbox/education/linux_device_drivers/ex01_simple_module.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/vbox/education/linux_device_drivers/ex01_simple_module.ko /home/vbox/education/linux_device_drivers/ex01_simple_module.o /home/vbox/education/linux_device_drivers/ex01_simple_module.mod.o
---
+++ .tmp_versions/ex01_simple_module.mod
@@ -0,0 +1,2 @@
+/home/vbox/education/linux_device_drivers/ex01_simple_module.ko
+/home/vbox/education/linux_device_drivers/ex01_simple_module.o
